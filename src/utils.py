924ac1b2f686ef671a5568297e11562beadeff26
kirpalsingh225 <kirpalsingh2252002@gmail.com>
2023-08-27 16:24:59 +0530

    update utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import os
 import dill
 import numpy as np
 import pandas as pd
+from sklearn.metrics import accuracy_score, recall_score, precision_score
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -13,3 +14,20 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+def evaluate_models(x_train, y_train, x_test, y_test, models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(x_train, y_train)
+            y_test_pred = model.predict(x_test)
+            accuracy = accuracy_score(y_test_pred, y_test)
+            recall = recall_score(y_test_pred, y_test)
+            precision = precision_score(y_test_pred, y_test)
+            report[list(models.keys())[i]] = precision
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
+
