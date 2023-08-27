b171c69bb53faa8d9a6e7e378916688eba1f5ccb
kirpalsingh225 <kirpalsingh2252002@gmail.com>
2023-08-27 11:12:05 +0530

    update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,15 @@
 import sys
+import os
+import dill
+import numpy as np
+import pandas as pd
+from src.exception import CustomException
 
-def error_message_detail(error, error_detail:sys):
-    _,_,exe_tb = error_detail.exc_info()
-    file_name = exe_tb.tb_frame.f_code.co_filename
-    error_message = f"error occured in python script name {file_name} line number {exe_tb.tb_lineno} error message {error}"
-    return error_message
-
-class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
-        self.error_message = error_message_detail(error_messagerror_detail=error_detail)
-
-    def __str__(self):
-        return self.error_message
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
