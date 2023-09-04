08d19205c6bdc051dcaecb9a00c9803a67128fb7
kirpalsingh225 <kirpalsingh2252002@gmail.com>
2023-09-04 09:13:55 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-application = Flask(__name__)
-app = application
+app = Flask(__name__)
 
 ## Route for home page
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 
-@app.route("/predict", methods=["GET", "POST"])
-def predict_datapoints():
-    if request.method=="GET":
-        return render_template("home.html")
+@app.route("/precision/<int:pre>")
+def show(pre):
+    return "the precision was" + str(pre)
+
+@app.route("/predict")
+def predict():
+    return render_template("predict.html")
+
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    val = 0
+    if request.method=="POST":
+        dur = float(request.form["duration"])
+        val = dur
+    return redirect(url_for("precision", score = val))    
+
+if __name__=="__main__":
+    app.run(debug=True)
+
